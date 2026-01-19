02162ad6ca089ba4a2bbef1d1efac79bb6bb5d3b
PauloBarco <paulo.barco34@gmail.com>
2026-01-19 13:21:42 -0300

    teste# Please enter the commit message for your changes. Lines starting

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,27 @@
-import { Button } from "../components/ui/button";
+import Header from "@/components/ui/header";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { Search } from "lucide-react";
 
 const Home = () => {
   return (
     <div>
       {/* header */}
       <Header />
+
+      <div className="p-5">
+        <h2 className="text-xl font-bold">Olá, Paulo</h2>
+        <p>Segunda-feira, 19 de janeiro.</p>
+
+        <div className="flex items-center gap-2">
+          <Input placeholder="Faça sua Busca..." />
+          <Button size="icon">
+            <Search className="h-4 w-4" />
+          </Button>
+        </div>
+      </div>
     </div>
   );
-
-  export default Home;
 };
+
+export default Home;
