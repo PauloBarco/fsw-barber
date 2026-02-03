39d5a50ee55bf60bb062af5167ad26f570c281c5
PauloBarco <paulo.barco34@gmail.com>
2026-02-03 13:49:02 -0300

    feat: nova entragada

--- components/ui/barbershop-item.tsx
+++ components/ui/barbershop-item.tsx
@@ -1,11 +1,19 @@
 import { Barbershop } from "@prisma/client";
+import { CardContent } from "./card";
 
 interface BarbershopItemProps {
   barbershop: Barbershop;
 }
 
 const BarbershopItem = ({ barbershop }: BarbershopItemProps) => {
-  return <h1>{barbershop.name}</h1>;
+  return (
+    <card>
+      <CardContent>
+        <div className="relative h-[159px]">
+          <Image fil className="object-cover" src={barbershop.imageUrl}/>
+        </div>
+      </CardContent>
+    </card>
 };
 
 export default BarbershopItem;
