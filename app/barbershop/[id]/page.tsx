f3f3ea6df708a5e7210f12cb8d71fd593ada8875
PauloBarco <paulo.barco34@gmail.com>
2026-02-12 15:56:03 -0300

    feat: add component services barber

--- app/barbershop/[id]/page.tsx
+++ app/barbershop/[id]/page.tsx
@@ -1,8 +1,11 @@
 import { db } from "@/app/_lib/prisma";
+import PhoneItem from "@/components/phone-item";
+import ServiceItem from "@/components/service-item";
 import { Button } from "@/components/ui/button";
 import { ChevronLeftIcon, MapPinIcon, MenuIcon, StarIcon } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
+import { notFound } from "next/navigation";
 
 const BarbershopPage = async ({
   params,
@@ -13,7 +16,14 @@ const BarbershopPage = async ({
 
   const barbershop = await db.barbershop.findUnique({
     where: { id },
+    include: {
+      services: true,
+    },
   });
+
+  if (!barbershop) {
+    return notFound();
+  }
 
   return (
     <div>
@@ -44,17 +54,40 @@ const BarbershopPage = async ({
         </Button>
       </div>
 
+      {/*Título*/}
       <div className="p-5 border-b border-solid">
         <h1 className="text-xl font-bold mb-3">{barbershop?.name}</h1>
-        <div className="flex items-center gap-1">
+        <div className="flex items-center gap-2">
           <MapPinIcon className="text-primary" size={18} />
           <p className="text-sm">{barbershop?.address}</p>
         </div>
 
-        <div className="flex items-center gap-1">
+        <div className="flex items-center gap-2">
           <StarIcon className="text-primary fill-primary" size={18} />
           <p className="text-sm">5,0 (499 avaliações)</p>
         </div>
+      </div>
+
+      {/*Descrição*/}
+      <div className="p-5 border-b border-solid space-y-2">
+        <h2 className="text-xs font-bold uppercase text-gray-400">Sobre nós</h2>
+        <p className="text-sm text-justify">{barbershop?.description}</p>
+      </div>
+      {/*Serviços*/}
+      <div className="space-y-3 border-b border-solid p-5">
+        <h2 className="text-xs font-bold uppercase text-gray-400">Serviços</h2>
+        <div className="space-y-3">
+          {barbershop?.services.map((service) => (
+            <ServiceItem key={service.id} service={service} />
+          ))}
+        </div>
+      </div>
+
+      {/* Contato */}
+      <div className="p-5 space-y-3">
+        {barbershop.phones.map((phone) => (
+          <PhoneItem key={phone} phone={phone} />
+        ))}
       </div>
     </div>
   );
---
+++ components/phone-item.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import { SmartphoneIcon } from "lucide-react";
+import { Button } from "./ui/button";
+
+interface PhoneItemProps {
+  phone: string;
+}
+
+const PhoneItem = ({ phone }: PhoneItemProps) => {
+  const handleCopyPhoneClick = async () => {
+    await navigator.clipboard.writeText(phone);
+  };
+
+  return (
+    <div className="flex justify-between">
+      {/* Esquerda */}
+      <div className="flex items-center gap-2">
+        <SmartphoneIcon size={18} />
+        <p className="text-sm">{phone}</p>
+      </div>
+
+      {/* Direita */}
+      <Button variant="outline" size="sm" onClick={handleCopyPhoneClick}>
+        Copiar
+      </Button>
+    </div>
+  );
+};
+
+export default PhoneItem;
