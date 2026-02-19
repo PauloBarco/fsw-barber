0b59ab9efde98b4675d49372ffc1f56f6d6ea25c
PauloBarco <paulo.barco34@gmail.com>
2026-02-19 12:54:49 -0300

    feat: add sidebar

--- components/ui/header.tsx
+++ components/ui/header.tsx
@@ -1,15 +1,17 @@
 import Image from "next/image";
 import { Card, CardContent } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
-import { CalendarIcon, HomeIcon, MenuIcon } from "lucide-react";
+import { CalendarIcon, HomeIcon, LogOutIcon, MenuIcon } from "lucide-react";
 import {
   Sheet,
   SheetTrigger,
   SheetContent,
   SheetHeader,
   SheetTitle,
+  SheetClose,
 } from "@/components/ui/sheet";
 import { quickSearchOptions } from "@/app/_constants/search";
+import { Avatar, AvatarImage } from "@/components/ui/avatar";
 
 const Header = () => {
   return (
@@ -24,17 +26,30 @@ const Header = () => {
             </Button>
           </SheetTrigger>
 
-          <SheetContent>
+          <SheetContent className="overflow-y-auto">
             <SheetHeader>
               <SheetTitle className="text-left">Menu</SheetTitle>
             </SheetHeader>
 
+            <div className="py-5 border-b flex items-center border-solid gap-3">
+              <Avatar>
+                <AvatarImage src="https://images.unsplash.com/photo-1608889175123-8ee362201f81?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NjB8fGF2YXRhcnxlbnwwfHwwfHx8MA%3D%3D" />
+              </Avatar>
+
+              <div>
+                <p className="font-bold">Paulo Barco</p>
+                <p className="text-xs">paulo.barco34@gmail.com</p>
+              </div>
+            </div>
+
             {/* Menu principal */}
             <div className="flex flex-col gap-2 py-5 border-b border-solid">
-              <Button variant="ghost" className="justify-start gap-2">
-                <HomeIcon size={18} />
-                Início
-              </Button>
+              <SheetClose asChild>
+                <Button variant="ghost" className="justify-start gap-2">
+                  <HomeIcon size={18} />
+                  Início
+                </Button>
+              </SheetClose>
 
               <Button variant="ghost" className="justify-start gap-2">
                 <CalendarIcon size={18} />
@@ -58,6 +73,13 @@ const Header = () => {
                   {option.title}
                 </Button>
               ))}
+            </div>
+
+            <div className="flex flex-col gap-2 py-5">
+              <Button variant="ghost" className="justify-start gap-2">
+                <LogOutIcon size={18} />
+                Sai da Conta
+              </Button>
             </div>
           </SheetContent>
         </Sheet>
